4c641eade9104a90dbaf58aa49b93d5ed0b4a39c
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2022-07-19 09:33:41 +0200

    Repository structure

---
+++ DemoAnalyzer/python/ConfFile_cfg.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+'root://eospublic.cern.ch//eos/opendata/cms/Run2015D/SingleElectron/MINIAOD/08Jun2016-v1/10000/001A703B-B52E-E611-BA13-0025905A60B6.root'
+)
+)
+
+process.demo = cms.EDAnalyzer('DemoAnalyzer'
+)
+
+
+process.p = cms.Path(process.demo)
